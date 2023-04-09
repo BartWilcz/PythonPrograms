7be3fa80c6ac27b98a7ce49208f5136ab57149ba
Bartosz <127223216+BartWilcz@users.noreply.github.com>
2023-04-09 15:21:00 +0200

    Update notification_manager.py

--- flight search app/notification_manager.py
+++ flight search app/notification_manager.py
@@ -2,13 +2,13 @@ import smtplib
 from twilio.rest import Client
 import os
 
-TWILIO_SID = os.get_env("TWILIO_SID")#"ACf19b4d58d7f922ea01d6f952c2a103b8"
-TWILIO_AUTH_TOKEN =  os.get_env("TWILIO_AUTH_TOKEN")     #"e689fb8ac7edc2c592383a25a8bc8157"
-TWILIO_VIRTUAL_NUMBER = os.get_env("TWILIO_VIRTUAL_NUMBER")     #"+12708354704"
-TWILIO_VERIFIED_NUMBER = os.get_env("TWILIO_VERIFIED_NUMBER")     #"+48533555882"
+TWILIO_SID = os.get_env("TWILIO_SID")
+TWILIO_AUTH_TOKEN =  os.get_env("TWILIO_AUTH_TOKEN")    
+TWILIO_VIRTUAL_NUMBER = os.get_env("TWILIO_VIRTUAL_NUMBER")    
+TWILIO_VERIFIED_NUMBER = os.get_env("TWILIO_VERIFIED_NUMBER")   
 
-my_email = os.get_env("EMAIL")     #"barpython28@gmail.com"
-password = os.get_env("EMAIL_PASS")      #"ddeldmtesnomktan"
+my_email = os.get_env("EMAIL")     
+password = os.get_env("EMAIL_PASS")
 
 class NotificationManager:
 
